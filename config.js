bf50fa2969917b7d3d572b5d9fbb213ac0a42739
LiteMasteralex <alexlara_97@hotmail.com>
2019-11-14 13:12:40 -0600

    Change database connection name

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/BlogDB";
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/alejandroLara-blog-post";
 exports.PORT = process.env.PORT || 8080;
